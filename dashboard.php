11eff4f04f44e7470957f2c4c9c13c675a020b4d
mariaeduardacampos <duda.akiyama@gmail.com>
2023-10-16 14:29:45 -0300

    página prontinha

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,21 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["usuario"])) {
+    header("Location: login.php");
+    exit;
+}
+
+echo "Bem-vindo, " . $_SESSION["usuario"] . "! Esta é  a página de dashboard.";
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
 </head>
 <body>
-    <h1> Dashboard </h1>
+<a href="logout.php"> Sair </a>
 </body>
 </html>
