7544ead88d79e9a3b63895f6d46268724d68aae8
mariaeduardacampos <duda.akiyama@gmail.com>
2023-10-20 12:53:27 -0300

    terminado

--- login.php
+++ login.php
@@ -1,11 +1,40 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
+
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=autenticacao", "root", "");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+    } catch (PDOExpection $e) {
+        die ("Erro na conexão com o banco de dados: " . $e->getMessage( 
+        ));
+    }
+    //verifique se o usuário existe e a senha está correta
+    $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE usuario = ?");
+    $stmt->execute([$usuario]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($senha, $user["senha"])) {
+        $_SESSION["usuario"] = $usuario;
+        header("Location: dashboard.php");
+    } else {
+        echo "<script>alert('Login falhou. Verifique suas credenciais.')<?script>";
+    }
+}
+?>
 <!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de login</title>
+<html>
+    <head>
+        <title>Login</title>
 </head>
 <body>
-    <h1> Tela de login </h1>
-</body>
+    <h2>Login</h2>
+    <form method="post">
+        <input type="text" name="usuario" placeholder="Nome de Usuário" required><br>
+        <input type= "password" name="senha" placeholder="Senha" required><br>
+        <input type="submit" value="Entrar">
+</form>
 </html>
--- registro.php
+++ registro.php
@@ -1,11 +1,39 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST["usuario"];
+    $senha = password_hash($_POST["senha"], PASSWORD_BCRYPT);
+    $email = $_POST["email"];
+
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=autenticacao", "root", "");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOExcpetion $e) {
+        die ("Erro na conexão com o banco de dados: " . $e->getMessage( ));
+    }
+
+    $smmt = $pdo->prepare("INSERT INTO usuarios (usuario, senha, email) VALUES (?, ?, ?)");
+    $smmt->execute([$usuario, $senha, $email]);
+
+    $_SESSION["usuario"] = $usuario;
+    header("location: dashboard.php");
+}
+?>
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de registro</title>
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Cadastro</title>
 </head>
 <body>
-    <h1> Tela de registro </h1>
+    <h1> TELA DE CADASTRO </H1>
+    <form method="post">
+        <input type= "text" name="usuario" placeholder="nome de usuario" required><br>
+        <input type= "password" name="senha" placeholder="senha" required><br>
+        <input type= "email" name="email" placeholder="email" required><br>
+        <input type= "submit" value= "Cadastrar">
+    </form>
 </body>
 </html>
